0ca90e27b48e9d29071150e7588578dade6dce90
Iglek Kuat <dizinnes228@gmail.com>
2020-06-11 01:43:46 +0600

    search-vk: add group name and posts wall parser

--- parser directory/parser.py
+++ parser directory/parser.py
@@ -3,16 +3,16 @@ import time
 from selenium import webdriver
 from bs4 import BeautifulSoup as bs
 
-#url = input('Enter a VK user: ')
+# url = input('Enter a VK user: ')
 url = 'https://vk.com/aimechkooo'
 
 
 def selenium_browser() -> webdriver:
     browser = webdriver.Chrome()
     browser.get(url)
-    #element = browser.find_element_by_css_selector('.noselect')
-    #element = browser.find_element_by_id('profile_groups_link')
-    #browser.execute_script('arguments[0].click();', element)
+    element = browser.find_element_by_css_selector('.header_label.fl_l')
+    # element = browser.find_element_by_id('profile_groups_link')
+    browser.execute_script('arguments[0].click();', element)
     return browser
 
 
@@ -20,21 +20,32 @@ def parsing() -> None:
     html = selenium_browser().page_source
     soup = bs(html, 'html.parser')
     try:
-
         title = soup.select_one('title').text
-        print(title)
+        print('Title: ' + title)
+
 
         profile_short_info = soup.select_one('.profile_info.profile_info_short').text
-        print(profile_short_info)
+        print('Short a info: ' + profile_short_info)
 
         profile_full_info = soup.select_one('.profile_info.profile_info_full').text
-        print(profile_full_info)
+        print('Full info' + profile_full_info)
+
+        counts_module = soup.select_one('.counts_module').text
+        print('All friends followers and etc: ' + counts_module)
+
+        following = soup.select_one('span.header_count.fl_l').text
+        print('Following: ' + following)
+
+        group_name = soup.select_one('.module_body.clear_fix').text
+        print('Group name: ' + group_name)
+
+        posts_on_wall = soup.select_one('.wall_text').text
+        print(posts_on_wall)
 
         status = soup.select_one('.current_text').text
-        print(status)
+        print('Status:' + status)
     except:
         pass
-
 
 
 if __name__ == '__main__':
